5c19c24d83310cbd22573c93f6ed623fe1c2c162
James Finkelstein <jiflotr9@gmail.com>
2024-03-28 01:18:34 -0500

    p1 finished, repo analyzer, gems installed

---
+++ p1.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+
+username = 'KonradIT'
+url = "https://api.github.com/users/#{username}/repos"
+response = HTTParty.get(url)
+
+if response.code == 200
+  repositories = JSON.parse(response.body)
+  most_starred_repo = repositories.max_by { |repo| repo['stargazers_count'] }
+
+  puts "Most Starred Repository:"
+  puts "Name: #{most_starred_repo['name']}"
+  puts "Description: #{most_starred_repo['description'] || 'N/A'}"
+  puts "Stars: #{most_starred_repo['stargazers_count']}"
+  puts "URL: #{most_starred_repo['html_url']}"
+end
+
+# This reepo I got from Capstone, couldn't end up using it for the project there, so I used it here :)
